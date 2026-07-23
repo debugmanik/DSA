8517cb5e95ebd6ba80e01d8fa7cdde904a269594
Manik Jain <manikjain28july@gmail.com>
2026-07-23 16:06:50 +0530

    2396-strictly-palindromic-number

--- LeetCode/Medium/2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ LeetCode/Medium/2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
+ bool ispalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i<j) {
+            if (s[i]!=s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
     bool isStrictlyPalindromic(int n) {
-        return false;
+       for (int i=2;i<=n-2;i++) {
+            int num = n;
+            string s = "";
+            while (num>0) {
+                s+=to_string(num % i);
+                num/=i;
+            }
+            if (!ispalindrome(s))
+                return false;
+        }
+        return true;
     }
 };
