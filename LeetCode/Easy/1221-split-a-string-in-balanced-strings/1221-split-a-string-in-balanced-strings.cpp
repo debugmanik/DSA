f46383cd5cd2470ba113f0180e9157d250014a19
Manik Jain <manikjain28july@gmail.com>
2026-07-24 17:00:17 +0530

    1221-split-a-string-in-balanced-strings

---
+++ LeetCode/Easy/1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int l=0,r=0,ans=0;
+        for (int i=0;i<s.size();i++) {
+            if (s[i]=='L')
+                l++;
+            else
+                r++;
+            if (l==r)
+                ans++;
+        }
+
+        return ans;
+    }
+};
