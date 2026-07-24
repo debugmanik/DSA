9dc71bd94cbcd17be98d3e8c0862b2d1a3b3c017
Manik Jain <manikjain28july@gmail.com>
2026-07-24 15:32:12 +0530

    1678-goal-parser-interpretation

--- LeetCode/Easy/1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ LeetCode/Easy/1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    string interpret(string s) {
-        string res = "";
-        for (int i=0; i<s.size();i++) {
-            if (s[i]=='G') {
+    string interpret(string command) {
+         string res= "";
+        for (int i=0;i<command.size();i++) {
+            if (command[i]=='G') {
                 res+='G';
             }
-            else if (s[i]=='(' && s[i+1]==')') {
+            else if (command[i]=='(' && command[i+1]==')') {
                 res+='o';
-                i++;          
+                i++;
             }
-            else if (s[i] =='('&& s.substr(i, 4)=="(al)") {
+            else {
                 res+="al";
-                i+=3;  
+                i+= 3;
             }
         }
         return res;
