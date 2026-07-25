5f95309d4fa35ff1aef5b6fdf077f8a7bbfd576e
Manik Jain <manikjain28july@gmail.com>
2026-07-26 02:35:00 +0530

    0557-reverse-words-in-a-string-iii

---
+++ LeetCode/Easy/0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+     stringstream ss(s);
+           string token = "";
+                string result = "";
+            while(ss >> token) {
+                reverse(begin(token), end(token));
+              result+=token + " ";
+}
+return result.substr(0, result.length()-1);
+    }
+};
