639164ce3c23f30113b078991f06e3fdc0cff80d
Manik Jain <manikjain28july@gmail.com>
2026-07-26 22:59:14 +0530

    1347-minimum-number-of-steps-to-make-two-strings-anagram

---
+++ LeetCode/Medium/1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        map<char, int> mp;
+        for(char c : s)
+            mp[c]++;
+        for(char c : t)
+            mp[c]--;
+        int ans = 0;
+        for(auto it : mp) {
+            if(it.second > 0)
+                ans += it.second;
+        }
+        return ans;
+    }
+};
