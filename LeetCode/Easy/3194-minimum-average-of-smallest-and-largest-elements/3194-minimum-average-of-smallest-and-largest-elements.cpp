b14b3db9fafc75923c272932b6bc0a5f28d70e83
Manik Jain <manikjain28july@gmail.com>
2026-07-31 14:33:06 +0530

    3194-minimum-average-of-smallest-and-largest-elements

---
+++ LeetCode/Easy/3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double minimumAverage(vector<int>& nums) {
+       sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<double>avg;
+        for (int i=0;i<n/2;i++) {
+            avg.push_back((nums[i]+nums[n - i - 1])/2.0);
+        }
+        return *min_element(avg.begin(),avg.end());
+    }
+};
