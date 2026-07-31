255c73442a9c88b1f1d723a9993d66de1251add3
Manik Jain <manikjain28july@gmail.com>
2026-07-31 14:20:05 +0530

    2578-split-with-minimum-sum

---
+++ LeetCode/Easy/2578-split-with-minimum-sum/2578-split-with-minimum-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int splitNum(int num) {
+        string s=to_string(num);
+        sort(s.begin(),s.end());
+        int n=s.size();
+        string a = "", b = "";
+        for(int i=0;i<s.size();i++){
+     if (i%2==0)
+                a+=s[i];
+            else
+                b+=s[i];
+    }
+            return stoi(a)+stoi(b);
+    }
+};
